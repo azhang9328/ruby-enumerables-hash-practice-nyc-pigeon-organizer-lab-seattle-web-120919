e85b7bb43c46b2d4678fd24c7cd98d56d1cc753d
Allen Zhang <azhang9328@gmail.com>
2019-12-08 01:02:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   pigeonhash = {}
+<<<<<<< HEAD
   data.each_pair do |mainkey, value|
 
     value.each_pair do |innerkey, namearr|
@@ -19,6 +20,19 @@ def nyc_pigeon_organizer(data)
         pigeonhash[name][mainkey].push(innerkey.to_s)
       end   
  
+=======
+  data.each_pair do |key, value|
+    p key
+    p value
+    value.each_pair do |key2, value2|
+    p key2
+    p value2
+    
+      value2.each do |name|
+      
+      pigeonhash[name] = {}
+      
+>>>>>>> ffea8a28a27a08cd6ee8489ef1af7dc8e3b0f64c
       end   
     end 
   end   
